2f8e2299353ad2a221a7c7d21db9c2ed95a09d52
ROECO <jesiah.roe@gmail.com>
2025-09-09 19:50:49 -0400

    minor fixes and additions

--- src/main/java/com/roeco/portfoliobackend/src/entity/Projects.java
+++ src/main/java/com/roeco/portfoliobackend/src/entity/Projects.java
@@ -1,5 +1,6 @@
 package com.roeco.portfoliobackend.src.entity;
 
+import jakarta.annotation.Nonnull;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -10,21 +11,26 @@ import java.util.List;
 @Table(name = "projects")
 @NoArgsConstructor
 @AllArgsConstructor
-@Data
+@Getter
+@Setter
+@ToString
 public class Projects {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
 
     @Column(unique = true, nullable = false)
+    @Nonnull
     private String name;
 
     @Column(length = 255, nullable = false)
+    @Nonnull
     private String description;
 
     @ElementCollection
     @CollectionTable(name = "tools_languages", joinColumns = @JoinColumn(name = "project_id"))
-    @Column(name = "tool_language")
+    @Column(name = "tool_language", nullable = false)
+    @OrderColumn(name = "position") // <- add if order matters
     private List<String> toolsLanguages = new ArrayList<>();
 
     @Column(length = 255)
--- src/main/java/com/roeco/portfoliobackend/src/repo/IProjectsRepo.java
+++ src/main/java/com/roeco/portfoliobackend/src/repo/IProjectsRepo.java
@@ -3,6 +3,7 @@ package com.roeco.portfoliobackend.src.repo;
 import com.roeco.portfoliobackend.src.entity.Projects;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+
 public interface IProjectsRepo extends JpaRepository<Projects,Integer> {
     Integer id(Integer id);
 }
